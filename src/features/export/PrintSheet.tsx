7defdd426ae4845e7b1b578913089b1991c6caf3
Leong Chee Fai <leongcheefai0521@gmail.com>
2026-04-20 18:31:40 +0800

    fix: remove browser print headers/footers, center PocketPages title

--- src/features/export/PrintSheet.tsx
+++ src/features/export/PrintSheet.tsx
@@ -15,6 +15,7 @@ export function PrintSheet() {
 
   return (
     <div id="print-sheet" style={{ display: 'none' }}>
+      <div id="print-title" style={{ display: 'none' }}>POCKET PAGES</div>
       {pages.map((pageIds, pageIdx) => (
         <div
           key={pageIdx}
